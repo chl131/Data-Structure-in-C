0fa11fef513de0ec89b8b022ab68e4e78a9378c8
CY <cyclonelee24@gmail.com>
2023-02-13 22:42:53 -0800

    changed the logic in credit.c

--- credit/credit.c
+++ credit/credit.c
@@ -1,109 +1,89 @@
-// #include <cs50.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <math.h>
 
-int checkSum(long long creditCard, int cardLength, int* mulTwoVal){
-    int sumOne = 0;
-    int sumTwo = 0;
-    int digit;
-    int idx = 0;
+enum CardType {INVALID = 0, AMEX, MASTERCARD, VISA};
 
-    while (creditCard > 0){
-        digit = creditCard % 10;
-        
-        if (idx % 2 == 0){
-            sumTwo += digit;
+enum CardType validCreditCards(int* C){
+    enum CardType cardType;
+    int8_t mulTwoVal[10] = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9}; // {0*2, 1*2, 2*2, 3*2, 4*2, 1+0, 1+2, 1+4, 1+6, 1+8}
+    int8_t i = 0;
+    uint8_t bucket0_0 = 0;
+    uint8_t bucket0_1 = 0;
+    uint8_t bucket1_0 = 0;
+    uint8_t bucket1_1 = 0;
+    int8_t flag = 0;
+    uint8_t checkSum = 0;
+
+    // check prefix
+    if (C[0] < 3 || C[0] > 5){
+        return INVALID;
+    }
+
+    if (C[0] == 5){
+        if (C[1] < 1 || C[1] > 5){
+            return INVALID;
         } else {
-            sumOne += mulTwoVal[digit];
+            cardType = MASTERCARD;
         }
-
-        creditCard /= 10;
-        idx ++;
-    }
-
-    if ((sumOne + sumTwo) % 10 == 0){
-        return 1;
+    } else if (C[0] == 3){
+        if (!(C[1] == 4 || C[1] == 7)){
+            return INVALID;
+        } else {
+            cardType = AMEX;
+        }
     } else {
-        return 0;
+        cardType = VISA;
     }
+    // finish check prefix
 
-}
-
-void validCreditCards(long long* creditCards, int arrSize){
-    // char cartType[4][16];
-    // strcpy(cartType[0], "AMEX");
-    // strcpy(cartType[1], "MASTERCARD");
-    // strcpy(cartType[2], "VISA");
-    // strcpy(cartType[3], "INVALID");
-    int mulTwoVal[10] = {0, 2, 4, 6, 8, 1, 3, 5, 7, 9}; // {0*2, 1*2, 2*2, 3*2, 4*2, 1+0, 1+2, 1+4, 1+6, 1+8}
-    int cardLength;
-    long long creditCard;
-    int prefix;
-    int isValid;
-
-    for(int i = 0; i < arrSize; i++){
-        
-        if (creditCards[i] != -1){
-            creditCard = creditCards[i];
-            isValid = 0;
-
-            cardLength = ceil(log10(creditCard));
-
-            if (creditCard < 0 || checkSum(creditCard, cardLength, mulTwoVal) == 0){
-                printf("Card Number: %lld ", creditCard);
-                printf("Type: %s\n", "INVALID");
-                printf("\n");
-                continue;
-            }
-
-            if (cardLength == 15){
-                prefix = creditCard / pow(10, cardLength-2);
-                if (prefix == 34 || prefix == 37){
-                    printf("Card Number: %lld ", creditCard);
-                    printf("Type: %s\n", "AMEX");
-                    printf("\n");
-                    isValid = 1;
-                }
-            } else if (cardLength == 16){
-                // MasterCard
-                prefix = creditCard / pow(10, cardLength-2);
-                if (prefix == 51 || prefix == 52 || prefix == 53 || prefix == 54 || prefix == 55){
-                    printf("Card Number: %lld ", creditCard);
-                    printf("Type: %s\n", "MASTERCARD");
-                    printf("\n");
-                    isValid = 1;
-                }
-                // VISA
-                prefix = creditCard / pow(10, cardLength-1);
-                if (prefix == 4){
-                    printf("Card Number: %lld ", creditCard);
-                    printf("Type: %s\n", "VISA");
-                    printf("\n");
-                    isValid = 1;
-                }
-            } else if (cardLength == 13){
-                prefix = creditCard / pow(10, cardLength-1);
-                if (prefix == 4){
-                    printf("Card Number: %lld ", creditCard);
-                    printf("Type: %s\n", "VISA");
-                    printf("\n");
-                    isValid = 1;
-                }
-            }
-
-            if (!isValid){
-                printf("Card Number: %lld ", creditCard);
-                printf("Type: %s\n", "INVALID");
-                printf("\n");
-            }
-            
+    // check sum and length
+    while (C[i] != -1 && i < 17){
+        if (flag == 0){
+            bucket0_0 += C[i];
+            bucket0_1 += mulTwoVal[C[i]];
+            flag = 1;
+        } else {
+            bucket1_0 += C[i];
+            bucket1_1 += mulTwoVal[C[i]];
+            flag = 0;
         }
+        i++;
     }
+
+    if (i > 16) return INVALID;
+
+    if ((i - 1) % 2 == 0){
+        checkSum = bucket0_0 + bucket1_1;
+    } else {
+        checkSum = bucket0_1 + bucket1_0;
+    }
+
+    if (checkSum % 10 != 0) return INVALID;
+
+    if (cardType == VISA && (i == 13 || i == 16)) return VISA;
+    if (cardType == AMEX && i == 15) return AMEX;
+    if (cardType == MASTERCARD && i == 16) return MASTERCARD;
+    return INVALID;
 }
 
 int main(void) 
 {   
-    long long creditCards[4] = {33333, 4003600000000014, -1, 6176292929};
-    validCreditCards(creditCards, 4);
+    int creditCards[32] = {4, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, -1};
+    enum CardType res = validCreditCards(creditCards);
+
+    if (!res){
+        // invalid format
+        printf("INVALID");
+        return 0;
+    }
+
+    if (res == AMEX){
+        printf("AMEX");
+    } else if (res == MASTERCARD){
+        printf("MASTERCARD");
+    } else if (res == VISA){
+        printf("VISA");
+    }
+
     return 0;
 }
